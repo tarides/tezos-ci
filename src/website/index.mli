b25e1ce0d3afdf5d2637ad3fed5d8035b875b5ca
Mirage CI <ci@mirage.io>
2021-11-22 14:49:39 +0000

    gitlab integration

--- src/website/index.mli
+++ src/website/index.mli
@@ -3,6 +3,10 @@ type t
 val make : unit -> t
 
 val update_state :
-  t -> id:string Current.t -> Lib.Task.subtask_node Current.t -> unit Current.t
+  t ->
+  source:Pipeline.Source.t ->
+  commit:string Current.t ->
+  Lib.Task.subtask_node Current.t ->
+  unit Current.t
 
 val routes : t -> Current_web.Resource.t Routes.route list
