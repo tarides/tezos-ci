3ab1d28b11b907041fdd08ae149c7ba692ef9eb7
Lucas Pluvinage <lucas@tarides.com>
2022-06-08 17:38:39 +0200

    packaging: opam definitions have moved

--- src/stages/packaging.ml
+++ src/stages/packaging.ml
@@ -17,23 +17,31 @@ let v ~package (tezos_repository : Analysis.Tezos_repository.t) =
     stage ~from
       ~child_builds:[ ("build_src", Lib.Fetch.spec tezos_repository) ]
       [
-        user ~uid:1000 ~gid:1000; 
+        user ~uid:1000 ~gid:1000;
         env "HOME" "/home/tezos";
         workdir "/tezos/";
         copy ~from:(`Build "build_src") [ "/tezos/" ] ~dst:"./";
-        run "./scripts/opam-pin.sh";
-        run "opam depext --yes %s" package;
+        (* from packaging.yml*)
+        run "git init _opam-repo-for-release";
+        run
+          "opam exec -- ./scripts/opam-prepare-repo.sh dev ./ \
+           ./_opam-repo-for-release";
+        run "git -C _opam-repo-for-release add packages";
+        run "git -C _opam-repo-for-release commit -m \"tezos packages\"";
+        (* from opam_template in templates.yml *)
+        run "opam remote add dev-repo ./_opam-repo-for-release";
+        run "opam depext --yes %s.dev" package;
         env "DUNE_CACHE" "enabled";
         env "DUNE_CACHE_TRANSPORT" "direct";
-        run ~cache "opam install --yes %s" package;
-        run ~cache "opam reinstall --yes --with-test %s" package;
+        run ~cache "opam install --yes %s.dev" package;
+        run ~cache "opam reinstall --yes --with-test %s.dev" package;
       ])
 
 let all ~builder (analysis : Tezos_repository.t Current.t) =
   let open Current.Syntax in
   let all_packages =
     let+ analysis = analysis in
-    analysis.bin_packages @ analysis.lib_packages
+    analysis.packages
   in
   Task.list_iter ~collapse_key:"packaging"
     (module struct
