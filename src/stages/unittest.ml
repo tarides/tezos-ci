eff619418a0ba20e645b9ff4d860de4095cad0a8
Lucas Pluvinage <lucas@tarides.com>
2022-06-08 17:17:53 +0200

    unittest: dune is used instead of make targets
    js tests: doesn't work with the default shell so bash is used

--- src/stages/unittest.ml
+++ src/stages/unittest.ml
@@ -1,7 +1,7 @@
 open Analysis
 open Lib
 
-let template ?(extra_script = []) ~targets analysis =
+let template ?(extra_script = []) ~name ~targets analysis =
   let build = Build.v analysis in
   let from =
     Variables.docker_image_runtime_build_test_dependencies analysis.version
@@ -10,13 +10,16 @@ let template ?(extra_script = []) ~targets analysis =
     match targets with
     | [] -> []
     | targets ->
-        [ Obuilder_spec.run "opam exec -- make %s" (String.concat " " targets) ]
+        [
+          Obuilder_spec.run "opam exec -- scripts/test_wrapper.sh %s %s" name
+            (String.concat " " targets);
+        ]
   in
   Obuilder_spec.(
     stage ~from
       ~child_builds:[ ("build", build); ("src", Lib.Fetch.spec analysis) ]
       ([
-         user ~uid:1000 ~gid:1000; 
+         user ~uid:1000 ~gid:1000;
          workdir "/home/tezos/src";
          copy ~from:(`Build "src") [ "/tezos/" ] ~dst:".";
          copy ~from:(`Build "build") [ "/dist/" ] ~dst:".";
@@ -30,37 +33,65 @@ let template ?(extra_script = []) ~targets analysis =
 
 let targets =
   [
-    ( "unit:010_PtGRANAD",
+    ( "unit:012_Psithaca",
       [
-        "src/proto_010_PtGRANAD/lib_client.test_proto";
-        "src/proto_010_PtGRANAD/lib_protocol.test_proto";
+        "@@src/proto_012_Psithaca/lib_protocol/test/integration/runtest";
+        "@src/proto_012_Psithaca/lib_protocol/test/integration/consensus/runtest";
+        "@src/proto_012_Psithaca/lib_protocol/test/integration/gas/runtest";
+        "@src/proto_012_Psithaca/lib_protocol/test/integration/michelson/runtest";
+        "@src/proto_012_Psithaca/lib_protocol/test/integration/operations/runtest";
+        "@src/proto_012_Psithaca/lib_protocol/test/pbt/runtest";
+        "@src/proto_012_Psithaca/lib_protocol/test/unit/runtest";
+        "@src/proto_012_Psithaca/lib_benchmark/runtest";
+        "@src/proto_012_Psithaca/lib_client/runtest";
+        "@src/proto_012_Psithaca/lib_plugin/runtest";
+        "@src/proto_012_Psithaca/lib_delegate/runtest";
       ],
       None );
-    ( "unit:011_PtHangz2",
+    ( "unit:013_PtJakart",
       [
-        "src/proto_011_PtHangz2/lib_benchmark/lib_benchmark_type_inference.test_proto";
-        "src/proto_011_PtHangz2/lib_benchmark.test_proto";
-        "src/proto_011_PtHangz2/lib_client.test_proto";
-        "src/proto_011_PtHangz2/lib_protocol.test_proto";
+        "@@src/proto_013_PtJakart/lib_protocol/test/integration/runtest";
+        "@src/proto_013_PtJakart/lib_protocol/test/integration/consensus/runtest";
+        "@src/proto_013_PtJakart/lib_protocol/test/integration/gas/runtest";
+        "@src/proto_013_PtJakart/lib_protocol/test/integration/michelson/runtest";
+        "@src/proto_013_PtJakart/lib_protocol/test/integration/operations/runtest";
+        "@src/proto_013_PtJakart/lib_protocol/test/pbt/runtest";
+        "@src/proto_013_PtJakart/lib_protocol/test/unit/runtest";
+        "@src/proto_013_PtJakart/lib_benchmark/runtest";
+        "@src/proto_013_PtJakart/lib_client/runtest";
+        "@src/proto_013_PtJakart/lib_plugin/runtest";
+        "@src/proto_013_PtJakart/lib_delegate/runtest";
       ],
       None );
     ( "unit:alpha",
       [
-        "src/proto_alpha/lib_benchmark/lib_benchmark_type_inference.test_proto";
-        "src/proto_alpha/lib_benchmark.test_proto";
-        "src/proto_alpha/lib_client.test_proto";
-        "src/proto_alpha/lib_protocol.test_proto";
+        "@@src/proto_alpha/lib_protocol/test/integration/runtest";
+        "@src/proto_alpha/lib_protocol/test/integration/consensus/runtest";
+        "@src/proto_alpha/lib_protocol/test/integration/gas/runtest";
+        "@src/proto_alpha/lib_protocol/test/integration/michelson/runtest";
+        "@src/proto_alpha/lib_protocol/test/integration/operations/runtest";
+        "@src/proto_alpha/lib_protocol/test/pbt/runtest";
+        "@src/proto_alpha/lib_protocol/test/unit/runtest";
+        "@src/proto_alpha/lib_protocol/test/regression/runtest";
+        "@src/proto_alpha/lib_benchmark/runtest";
+        "@src/proto_alpha/lib_client/runtest";
+        "@src/proto_alpha/lib_plugin/runtest";
+        "@src/proto_alpha/lib_delegate/runtest";
       ],
       None );
-    ("unit:non-proto", [ "test-nonproto-unit" ], None);
+    ( "unit:non-proto",
+      [],
+      Some
+        Obuilder_spec.
+          [ run "opam exec -- make test-nonproto-unit test-webassembly" ] );
     ( "unit:js_components",
       [],
       Some
         Obuilder_spec.
           [
             run
-              ". ./scripts/install_build_deps.js.sh && opam exec -- make \
-               test-js";
+              "bash -c \". ./scripts/install_build_deps.js.sh && opam exec -- make \
+               test-js\"";
           ] );
     ( "unit:protocol_compiles",
       [],
@@ -78,7 +109,7 @@ let all ~builder (analysis : Analysis.Tezos_repository.t Current.t) =
          let spec =
            let open Current.Syntax in
            let+ analysis = analysis in
-           template ?extra_script ~targets analysis
+           template ?extra_script ~name ~targets analysis
          in
          Lib.Builder.build ~label:name builder spec)
        targets)
