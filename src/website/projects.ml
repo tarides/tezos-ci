d4d45c114aa62de75f10150247016ae0e715a4be
Tim McGilchrist <timmcgil@gmail.com>
2022-06-14 14:19:51 +1000

    Listing owner and projects.

--- src/website/projects.ml
+++ src/website/projects.ml
@@ -1,4 +1,26 @@
+(* TODO
+   1. Copy styling from ocurrent_web github page
+   2. Convert sha to branch/ref name
+   3. Summarise build status 
+ *)
+
+let org_url owner =
+  Printf.sprintf "/gitlab/%s" owner
+
 module Index = Ocaml_ci.Index
+
+let breadcrumbs steps page_title =
+  let open Tyxml.Html in
+  let add (prefix, results) (label, link) =
+    let prefix = Printf.sprintf "%s/%s" prefix link in
+    let link = li [a ~a:[a_href prefix] [txt label]] in
+    (prefix, link :: results)
+  in
+  let _, steps = List.fold_left add ("", []) steps in
+  let steps = li [b [txt page_title]] :: steps in
+  ol ~a:[a_class ["breadcrumbs"]] (
+      List.rev steps
+    )
 
 let render_project_ci_ref project_owner project_name (name, hash) =
   let open Tyxml_html in
@@ -16,12 +38,27 @@ let handle_owner_project ~engine:_ owner project = object
         |> List.map (fun p -> li (render_project_ci_ref owner project p))
       in
       Current_web.Context.respond_ok ctx @@ [
+          breadcrumbs ["gitlab", "gitlab";
+                       owner, owner;
+                       project, project;] project;
           div [
               h3 [txt (Fmt.str "CI Refs for %s/%s" owner project)];
               ul refs
             ]
         ]
   end
+
+let format_org org =
+  let open Tyxml.Html in
+  li [a ~a:[a_href (org_url org)] [txt org]]
+
+let list_orgs ctx =
+  let orgs = Ocaml_ci_gitlab.Pipeline.gitlab_repos in
+  let body = Tyxml.Html.[
+        breadcrumbs [] "gitlab";
+        ul (List.map (fun org -> format_org org.Current_gitlab.Repo_id.name) orgs)
+             ] in
+  Current_web.Context.respond_ok ctx body
 
 let render_project (project : Current_gitlab.Repo_id.t) =
   let open Tyxml_html in
@@ -35,6 +72,7 @@ let handle_list_owners ~engine:_ = object
       let projects = Ocaml_ci_gitlab.Pipeline.gitlab_repos in
       
       Current_web.Context.respond_ok ctx @@ [
+          breadcrumbs [] "gitlab";
           div [
             h3 [ txt ("GitLab Projects:")]];
             ul (List.map (fun p -> li (render_project p)) projects)
@@ -44,8 +82,17 @@ let handle_list_owners ~engine:_ = object
 let handle_owner ~engine:_ owner = object
     inherit Current_web.Resource.t
     method! private get ctx =
-      (* TODO Fill in the owner projects we know about! *)
-      Current_web.Context.respond_ok ctx @@ [Tyxml_html.txt @@ owner ^ "not found"]
+      let open Tyxml_html in
+      let projects = Ocaml_ci_gitlab.Pipeline.gitlab_repos
+                     |> List.filter (fun (x :Current_gitlab.Repo_id.t) -> String.equal x.owner owner) in
+      
+      Current_web.Context.respond_ok ctx @@ [
+          breadcrumbs ["gitlab", "gitlab";
+                       owner, owner;] owner;
+          div [
+              h3 [ txt ("GitLab Projects:")]];
+          ul (List.map (fun p -> li (render_project p)) projects)
+        ]
   end
 
 let render_variant job_id (variant, job_state) =
